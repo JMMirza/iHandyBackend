cda31b8ec292ecf929b4e29b444cc77138c47008
JMMIrza <janmuhammadmirza@gmail.com>
2022-07-05 23:43:51 +0500

    changes

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ec2-44-198-82-71.compute-1.amazonaws.com',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'ihandy',
+  username: 'lmhiqdgislknqs',
+  password: 'a886e015d07c873612bae0caf142f69d81c28636d93b85da9f3bdc189158cfa8',
+  database: 'd59b3mpn5suo4k',
   autoLoadEntities: true,
   synchronize: true,
 };
