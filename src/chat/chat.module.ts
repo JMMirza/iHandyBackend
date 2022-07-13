e2405ba9760ad7e54727f114d57e02cb5628db82
JMMIrza <janmuhammadmirza@gmail.com>
2022-07-13 15:14:40 +0500

    changes

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,11 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ChatService } from './chat.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ChatRepository } from './repositories/chat.respository';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([ChatRepository])],
-  providers: [ChatService],
-  exports: [ChatService],
-})
+@Module({})
 export class ChatModule {}
